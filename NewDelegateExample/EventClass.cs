ab953239ee72ab7e54ea9be3adf3af0d8c89a7d7
Ani Avetisyan <aniavetissyan1987@gmail.com>
2023-07-13 11:32:12 +0400

    examples with event handler

---
+++ NewDelegateExample/EventClass.cs
@@ -0,0 +1,34 @@
+﻿using static NewDelegateExample.Class1;
+using static NewDelegateExample.ShopClass;
+
+namespace NewDelegateExample
+{
+    internal class EventClass
+    {
+        public event Class1Delegate ProcessEvent;
+
+        public event ShopDelegate ShopEvent;
+
+        public void StartProcess(ref int num)
+        {
+            Console.WriteLine("Process started");
+            OnProcessEvent(ref num);
+        }
+
+        public virtual void OnProcessEvent(ref int num)
+        {
+            ProcessEvent?.Invoke(ref num);
+        }
+
+        public void StartShopEvent()
+        {
+            Console.WriteLine("Shop Event Process Started");
+            OnShopEvent();
+        }
+        
+        public virtual void OnShopEvent()
+        {
+            ShopEvent?.Invoke();
+        }
+    }
+}
--- NewDelegateExample/Program.cs
+++ NewDelegateExample/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using NewDelegateExample;
+
+internal class Program
 {
     public delegate void MyDelegate(int num);
     private static void Main(string[] args)
@@ -23,15 +25,34 @@
         shopDel += shopClass.Delivered;
         shopDel();
 
+        ////////Event example for ShopDelegate/////////
+        EventClass myEvent = new EventClass();
+        myEvent.ShopEvent += shopClass.Order;
+        myEvent.ShopEvent += shopClass.Delivered;
+        myEvent.StartShopEvent();
 
         int x = 10;
+        int y = 20;
         Class1.Class1Delegate classDel = Class1.Method1;
         classDel += Class1.Method2;
         classDel += Class1.Method3;
         classDel(ref x);
 
+        //////  Event example for Class1Delegate   //////////
+       
+        myEvent.ProcessEvent += Class1.Method1;
+        myEvent.ProcessEvent += Class1.Method2;
+        myEvent.ProcessEvent += Class1.Method3;
+        myEvent.StartProcess(ref y);
+
 
 
         Console.WriteLine("Hello, World!");
+    }
+
+    public static void myEvent_ProcessEvent()
+    {
+        Console.WriteLine("Process Completed!");
+
     }
 }
